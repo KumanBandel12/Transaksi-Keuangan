33e9725ec5e0c59021e1abe08a13afad6cd089b4
KumanBandel12 <agambonanza1210@gmail.com>
2025-06-30 22:30:18 +0700

    Delete TransaksiKeuangan directory

--- TransaksiKeuangan/src/main/java/pbo/transaksikeuangan/saldoStrategy.java
+++
@@ -1,6 +0,0 @@
-package pbo.transaksikeuangan;
-import java.io.Serializable; // Pastikan ini ada
-interface saldoStrategy extends Serializable { // Pastikan 'extends Serializable' ada
-    void execute(Transaksi transaksi);
-    String getNama();
-}
